e6bff6db2090bcc7c94aed4c7323facf3a3d9da8
hidaheregte <heregte@gmail.com>
2025-07-29 20:40:06 +0800

    Add files via upload

---
+++ mapdata.js
@@ -0,0 +1,299 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    location_description: "",
+    location_url: "",
+    location_color: "#c42730",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: "20",
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    MN035: {
+      name: "Орхон",
+      description: " "
+    },
+    MN037: {
+      name: "Дархан-Уул",
+      description: "Дархан-Уул НЭ",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN039: {
+      name: "Хэнтий",
+      description: "<li>УАУ НЭ</li>",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN041: {
+      name: "Хөвсгөл",
+      description: " "
+    },
+    MN043: {
+      name: "Ховд",
+      description: "<li>Булган Cумын НЭ</li>",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN046: {
+      name: "Увс",
+      description: "Увс НЭ",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN047: {
+      name: "Төв",
+      description: "<li>Төв НЭ</li>\n  <li>Жаргалант Cэргээн 3асах Tөв</li>",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN049: {
+      name: "Сэлэнгэ",
+      description: "<li>Мандал Cумын НЭ</li>",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN051: {
+      name: "Сүхбаатар",
+      description: "Сүхбаатар НЭ",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN053: {
+      name: "Өмнөговь",
+      description: "<li>Өмнөговь БОЭТ</li>\n  <li>Аминбулаг</li>",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN055: {
+      name: "Өвөрхангай",
+      description: "<li>Ананд Хужирт Рашаан Сувилал</li>\n  <li>Хужирт Рашаан Сувилал</li>\n  <li>Өвөрхангай БОЭТ</li>",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN057: {
+      name: "Завхан",
+      color: "#015097",
+      hover_color: "#92cae4",
+      description: "<li>Завхан НЭ</li>\n  <li>Завхан УАУТ</li>"
+    },
+    MN059: {
+      name: "Дундговь",
+      description: " "
+    },
+    MN061: {
+      name: "Дорнод",
+      description: "Дорнод БОЭТ",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN063: {
+      name: "Дорноговь",
+      description: " "
+    },
+    MN064: {
+      name: "Говь-Сүмбэр",
+      description: " "
+    },
+    MN065: {
+      name: "Говь-Алтай",
+      description: "Говь-Алтай НЭ",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN067: {
+      name: "Булган",
+      description: "Булган НЭ",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN069: {
+      name: "Баянхонгор",
+      description: "Баянхонгор НЭ",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN071: {
+      name: "Баян-Өлгий",
+      description: "Баян-Өлгий НЭ",
+      color: "#015097",
+      hover_color: "#92cae4"
+    },
+    MN073: {
+      name: "Архангай",
+      color: "#015097",
+      hover_color: "#92cae4",
+      description: "Архангай НЭ"
+    },
+    MN1: {
+      name: "Улаанбаатар",
+      description: "<li>100 нас</li>\n  <li>Аддиктологийн төв</li>\n  <li>Агь-Манба</li>\n  <li>Ариун Лотус СЗТ</li>\n  <li>Асралт мед</li>\n  <li>Азимед</li>\n  <li>Aлергимед</li>\n  <li>Багахангай ЭМТ</li>\n  <li>БГД ЭМТ</li>\n  <li>Багануур ЭМТ</li>\n  <li>БЗД ЭМТ</li>\n  <li>БЗД НЭ</li>\n  <li>Бриллиант</li>\n  <li>УБТЗ ТЭ</li>\n  <li>ГССҮТ</li>\n  <li>Жаргалант СЗТ</li>\n  <li>Зуун Эрдэнэ</li>\n  <li>НААГ</li>\n  <li>Нандин Ухаан</li>\n  <li>Налайх ЭМТ</li>\n  <li>НТТТ</li>\n  <li>НХАГ</li>\n  <li>Одчим Наран</li>\n  <li>СЭМҮТ</li>\n  <li>СХД ЭМТ</li>\n  <li>СХД НЭ</li>\n  <li>СЗҮТ</li>\n  <li>ТТАХНЭ</li>\n  <li>Улаанбаатар мед</li>\n  <li>УХТЭ</li>\n  <li>Хан-Уул ЭМТ</li>\n  <li>ХӨСҮТ</li>\n  <li>ХУД НЭ</li>\n  <li>ХУД ЭМТ</li>\n  <li>ЦТЭ</li>\n  <li>Цу-Эрдэнэ</li>\n  <li>Эм Би Жи Мед</li>\n  <li>Энэрэл</li>\n  <li>ЭМЖЖ</li>\n  <li>ЭХЭМҮТ</li>\n  <li>SBNE</li>\n  <li>NURA</li>\n  <li>СБД ЭМТ</li>",
+      color: "#015097",
+      hover_color: "#92cae4"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Улаанбаатар",
+      lat: "47.916667",
+      lng: "106.916667"
+    }
+  },
+  labels: {
+    MN035: {
+      name: "Орхон",
+      parent_id: "MN035"
+    },
+    MN037: {
+      name: "Дархан-Уул",
+      parent_id: "MN037"
+    },
+    MN039: {
+      name: "Хэнтий",
+      parent_id: "MN039"
+    },
+    MN041: {
+      name: "Хөвсгөл",
+      parent_id: "MN041"
+    },
+    MN043: {
+      name: "Ховд",
+      parent_id: "MN043"
+    },
+    MN046: {
+      name: "Увс",
+      parent_id: "MN046"
+    },
+    MN047: {
+      name: "Төв",
+      parent_id: "MN047"
+    },
+    MN049: {
+      name: "Сэлэнгэ",
+      parent_id: "MN049"
+    },
+    MN051: {
+      name: "Сүхбаатар",
+      parent_id: "MN051"
+    },
+    MN053: {
+      name: "Өмнөговь",
+      parent_id: "MN053"
+    },
+    MN055: {
+      name: "Өвөрхангай",
+      parent_id: "MN055"
+    },
+    MN057: {
+      name: "Завхан",
+      parent_id: "MN057"
+    },
+    MN059: {
+      name: "Дундговь",
+      parent_id: "MN059"
+    },
+    MN061: {
+      name: "Дорнод",
+      parent_id: "MN061"
+    },
+    MN063: {
+      name: "Дорноговь",
+      parent_id: "MN063"
+    },
+    MN064: {
+      name: "Говь-Сүмбэр",
+      parent_id: "MN064"
+    },
+    MN065: {
+      name: "Говь-Алтай",
+      parent_id: "MN065"
+    },
+    MN067: {
+      name: "Булган",
+      parent_id: "MN067"
+    },
+    MN069: {
+      name: "Баянхонгор",
+      parent_id: "MN069"
+    },
+    MN071: {
+      name: "Баян-Өлгий",
+      parent_id: "MN071"
+    },
+    MN073: {
+      name: "Архангай",
+      parent_id: "MN073"
+    },
+    MN1: {
+      name: "Улаанбаатар",
+      parent_id: "MN1"
+    }
+  },
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
